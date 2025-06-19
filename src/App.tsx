9ca5dd2a750f803797e19c70b551c2eedb2cbd6e
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-06-19 15:14:07 +0300

    Add copyright header to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+/**
+ * Copyright (c) 2024 LocalPDF Team
+ * 
+ * This file is part of LocalPDF.
+ * 
+ * LocalPDF is proprietary software: you may not copy, modify, distribute,
+ * or use this software except as expressly permitted under the LocalPDF
+ * Source Available License v1.0.
+ * 
+ * See the LICENSE file in the project root for license terms.
+ * For commercial licensing, contact: license@localpdf.online
+ */
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary';
