8b469a1b5a6fdf824e50874475c9159fbf7da667
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-06-20 13:08:05 +0300

    Remove HelmetProvider temporarily to fix build
    - Remove HelmetProvider import and usage
    - This fixes CI build until package-lock.json is updated
    - SEO meta tags will use document.title for now

--- src/main.tsx
+++ src/main.tsx
@@ -13,17 +13,14 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </HelmetProvider>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
