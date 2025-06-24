3bf809932ad0ea684d92ff46e921099be929754a
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-06-24 19:18:26 +0300

    🧪 Temporarily use test app to isolate MIME issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 /**
- * LocalPDF - Main application entry point
+ * LocalPDF - Main application entry point (TEST MODE)
  */
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import { ErrorBoundary } from './components/ErrorBoundary'
-import App from './App.tsx'
-import './index.css'
+import TestApp from './TestApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </ErrorBoundary>
+    <TestApp />
   </StrictMode>,
 )
