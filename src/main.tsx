6c0265b85c376b4b41f09359283ac0b5f4c72974
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-06-24 19:24:42 +0300

    🔧 Remove CSS import that causes MIME error

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import App from './App.tsx'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
