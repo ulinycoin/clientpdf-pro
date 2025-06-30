eabbc8d93cf63c36e4c371cd926fb35760ceb96f
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-06-30 23:58:55 +0300

    feat(components): add Footer to organisms index exports

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header';
+export { default as Footer } from './Footer';
 export { default as ToolsGrid } from './ToolsGrid';
 export { default as MergeTool } from './MergeTool';
 export { default as CompressionTool } from './CompressionTool';
