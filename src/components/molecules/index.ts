1f4589bb4e6b5b94daa78cae2d0c6d627e53629c
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-07-03 00:07:00 +0300

    Fix molecules index - remove duplicate ToolCard and missing CompressionSettings

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,2 @@
 export { default as FileUploadZone } from './FileUploadZone';
 export { default as FileList } from './FileList';
-export { default as ToolCard } from './ToolCard';
-export { default as CompressionSettings } from './CompressionSettings';
