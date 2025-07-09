3deccbc21cc53bdefce4567f4153d3b27b6384ca
ulinycoin <156083970+ulinycoin@users.noreply.github.com>
2025-07-09 18:56:46 +0300

    feat(security): Update atoms index to include PasswordInput

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as ProgressBar } from './ProgressBar';
+export { default as PasswordInput } from './PasswordInput';
